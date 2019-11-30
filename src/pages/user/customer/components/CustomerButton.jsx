13e3df477764b55d86c150e962dbd48e05aa8717
gz <l2241883@gmail.com>
2019-11-30 16:56:20 +0800

    feat: add modal on addCoustomer

--- src/pages/user/customer/components/CustomerButton.jsx
+++ src/pages/user/customer/components/CustomerButton.jsx
@@ -2,40 +2,54 @@ import React, { Component } from 'react';
 import { Button, Icon, Modal } from 'antd';
 
 import { PathName } from '../config';
+import CustomerForm from './CustomerForm'
 import style from '../index.less';
 
 class CustomerButton extends Component {
   constructor(props) {
     super(props);
-    const { match } = this.props;
-    const { path } = match;
+    const { match } = this.props
+    const { path } = match
 
     this.state = {
       addInfo: '',
       visible: false,
+      formType: 0,
     };
 
     if (path === PathName.MENU_INFO) {
-      this.state.addInfo = '添加客户信息';
+      this.state.addInfo = '添加客户信息'
+      this.state.formType = 0
     }
     if (path === PathName.MENU_DEMANDS) {
-      this.state.addInfo = '添加客户需求进度';
+      this.state.addInfo = '添加客户需求进度'
+      this.state.formType = 1
     }
   }
 
-  onOpen = () => {};
+  onOpen = () => {
+    this.setState({
+      visible: true,
+    })
+  }
 
-  onCancel = () => {};
+  onCancel = () => {
+    this.setState({
+      visible: false,
+    })
+  }
 
   render() {
-    const { addInfo, visible } = this.state;
+    const { addInfo, visible, formType } = this.state;
     return (
       <div className={style.addItem}>
         <Button className={style.addItem} type="primary" onClick={this.onOpen}>
           <Icon type="add" />
           {addInfo}
         </Button>
-        <Modal title={addInfo} visible={visible} onCancel={this.onCancel}></Modal>
+        <Modal title={addInfo} visible={visible} onCancel={this.onCancel} footer={null} keyboard>
+           <CustomerForm formType={formType} />
+        </Modal>
       </div>
     );
   }
