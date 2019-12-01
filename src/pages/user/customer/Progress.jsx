230a1023b498baa677db1e3e7569b6ced14ce5eb
gz <l2241883@gmail.com>
2019-12-01 19:23:31 +0800

    modify the return object in info  &  demands component

--- src/pages/user/customer/Progress.jsx
+++ src/pages/user/customer/Progress.jsx
@@ -22,7 +22,7 @@ class Progress extends PureComponent {
       },
     ];
 
-    const operate = (text, record) => <TableOperation {...record} {...this.props} />;
+    const operate = (text, record) => <TableOperation record = {record} {...this.props} />;
 
     return (
       <div className={style.container}>
