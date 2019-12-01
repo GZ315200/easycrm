5781450c2a3f67e34c748216f66ded26b5686b76
gz <l2241883@gmail.com>
2019-12-01 19:41:15 +0800

    add customer info & demands api configs & methods

--- src/services/customer.js
+++ src/services/customer.js
@@ -1,9 +1,67 @@
 import request from '@/utils/request';
+import apiConfig from './apiConfig';
 
 export async function addCustomerInfo(params) {
-  return request('/api/customer/info', {
+  return request(apiConfig.CUSTOMER_INFO, {
     method: 'POST',
     data: params,
   });
 }
 
+export async function getAllCustomerInfo() {
+  return request(apiConfig.CUSTOMER_INFO, {
+    method: 'GET',
+  });
+}
+
+export async function deleteCustomerInfo(id) {
+  return request(`${apiConfig.CUSTOMER_INFO}/${id}`, {
+    method: 'DELETE',
+  });
+}
+
+export async function modifyCustomerInfo(params) {
+  return request(apiConfig.CUSTOMER_DEMANDS, {
+    method: 'PUT',
+    data: params,
+  });
+}
+
+export async function searchCustomerInfo(value) {
+    return request(`${apiConfig.CUSTOMER_INFO}?searchValue=${value}`, {
+      method: 'GET',
+    });
+}
+
+
+export async function addCustomerDemands(params) {
+  return request(apiConfig.CUSTOMER_DEMANDS, {
+    method: 'POST',
+    data: params,
+  });
+}
+
+export async function modifyCustomerDemands(params) {
+  return request(apiConfig.CUSTOMER_DEMANDS, {
+    method: 'PUT',
+    data: params,
+  });
+}
+
+export async function getAllCustomerDemands() {
+  return request(apiConfig.CUSTOMER_DEMANDS, {
+    method: 'GET',
+  });
+}
+
+export async function deleteCustomerDemands(id) {
+  return request(`${apiConfig.CUSTOMER_DEMANDS}/${id}`, {
+    method: 'DELETE',
+  });
+}
+
+export async function searchCustomerDemands(value) {
+    return request(`${apiConfig.CUSTOMER_DEMANDS}?searchValue=${value}`, {
+      method: 'GET',
+    });
+}
